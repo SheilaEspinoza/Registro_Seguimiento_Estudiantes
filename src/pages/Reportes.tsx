a9539664d85aeb5f5f136e8d873fac847410e8b0
SheilaEspinoza <sheilaespinoza43@gmail.com>
2025-07-26 01:50:19 -0500

    Cambio diseño FormEst

--- src/pages/Reportes.tsx
+++ src/pages/Reportes.tsx
@@ -1,39 +1,9 @@
 import "../App.css";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
-  ResponsiveContainer,
-} from "recharts";
 
-const estudiantes = [
-  { nombre: "Juan", promedio: 8.5 },
-  { nombre: "Ana", promedio: 9.0 },
-  { nombre: "Luis", promedio: 7.8 },
-  { nombre: "María", promedio: 8.9 },
-];
 
 const Reportes = () => {
   return (
-    <div className="reportes">
-      <h1>Reporte de Promedios</h1>
-
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart
-          data={estudiantes}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="nombre" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="promedio" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+    <h1>Reportes</h1>
   );
 };
 
