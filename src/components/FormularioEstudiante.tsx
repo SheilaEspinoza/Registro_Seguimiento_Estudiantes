9a98b7ccdd09efa71525ef9d6f5c0286f501f82f
SheilaEspinoza <sheilaespinoza43@gmail.com>
2025-07-26 13:54:58 -0500

    Ajuste tam Modal y Formulario

--- src/components/FormularioEstudiante.tsx
+++ src/components/FormularioEstudiante.tsx
@@ -7,7 +7,8 @@ interface Props {
 
 function FormularioEstudiante({ onAgregar }: Props) {
   const [activeTab, setActiveTab] = useState("personales");
-
+  
+  const [foto, setFoto] = useState<File | null>(null);
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
   const [cedula, setCedula] = useState("");
@@ -21,9 +22,24 @@ function FormularioEstudiante({ onAgregar }: Props) {
   const [carrera, setCarrera] = useState("");
   const [nivel, setNivel] = useState(1);
 
+  //campos para limpiar - se llama en boton limpiar
+  const limpiarCampos = () => {
+  setNombre("");
+  setApellido("");
+  setCedula("");
+  setCorreo("");
+  setFoto(null);
+  setCiudad("");
+  setDireccion("");
+  setTelf("");
+  setCarrera("");
+  setNivel(1);
+};
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onAgregar({ nombre, apellido, cedula, carrera, nivel });
+    setFoto(null);
     setNombre("");
     setApellido("");
     setCedula("");
@@ -40,6 +56,7 @@ function FormularioEstudiante({ onAgregar }: Props) {
   return (
     <div className="card p-4 mb-4">
       <h5 className="card-title">Registrar estudiante</h5>
+
       <ul className="nav nav-tabs mb-3">
         <li className="nav-item">
           <button
@@ -49,14 +66,7 @@ function FormularioEstudiante({ onAgregar }: Props) {
             Datos personales
           </button>
         </li>
-        <li className="nav-item">
-          <button
-            className={`nav-link ${activeTab === "ubicacion" ? "active" : ""}`}
-            onClick={() => setActiveTab("ubicacion")}
-          >
-            Datos de ubicación
-          </button>
-        </li>
+
         <li className="nav-item">
           <button
             className={`nav-link ${activeTab === "academicos" ? "active" : ""}`}
@@ -65,66 +75,95 @@ function FormularioEstudiante({ onAgregar }: Props) {
             Datos académicos
           </button>
         </li>
+        
+        <li className="nav-item">
+          <button
+            className={`nav-link ${activeTab === "ubicacion" ? "active" : ""}`}
+            onClick={() => setActiveTab("ubicacion")}
+          >
+            Datos de ubicación
+          </button>
+        </li>
       </ul>
 
       <form onSubmit={handleSubmit}>
       {activeTab === "personales" && (
-          <>
-            <div className="mb-3">
+        <div className="row">
+            <div className="col-md-6 mb-3">
+              <label className="form-label">Foto</label>
+              <input
+                type="file"
+                accept="image/*"
+                className="form-control form-control-sm"
+                onChange={(e) => setFoto(e.target.files?.[0] || null)}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
               <label className="form-label">Cédula</label>
-              <input className="form-control" value={cedula} onChange={(e) => setCedula(e.target.value)} />
+              <input className="form-control form-control-sm" value={cedula} onChange={(e) => setCedula(e.target.value)} />
             </div>
-            <div className="mb-3">
-              <label className="form-label">Nombres</label>
-              <input className="form-control" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+            <div className="col-md-6 mb-3">
+              <label className="form-label ">Nombres</label>
+              <input className="form-control form-control-sm" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
             </div>
-            <div className="mb-3">
+            <div className="col-md-6 mb-3">
               <label className="form-label">Apellidos</label>
-              <input className="form-control" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+              <input className="form-control form-control-sm" value={apellido} onChange={(e) => setApellido(e.target.value)} />
             </div>
-            <div className="mb-3">
+            <div className="col-md-6 mb-3">
               <label className="form-label">Correo</label>
-              <input className="form-control" value={correo} onChange={(e) => setCorreo(e.target.value)} />
+              <input className="form-control form-control-sm" value={correo} onChange={(e) => setCorreo(e.target.value)} />
             </div>
-          </>
+          </div>
         )}
 
          {activeTab === "academicos" && (
-          <>
-            <div className="mb-3">
+          <div className="row">
+            <div className="col-md-6 mb-3">
               <label className="form-label">Carrera</label>
-              <input className="form-control" value={carrera} onChange={(e) => setCarrera(e.target.value)} />
+              <select
+                className="form-select form-select-sm"
+                value={carrera}
+                onChange={(e) => setCarrera(e.target.value)}
+              >
+                <option value="">Seleccione una carrera</option>
+                <option value="Computación">Computación</option>
+                <option value="Civil">Civil</option>
+              </select>
             </div>
-            <div className="mb-3">
+            <div className="col-md-6 mb-3">
               <label className="form-label">Nivel</label>
               <input 
                 type="number" 
-                className="form-control" 
+                className="form-control form-control-sm" 
                 value={nivel} 
                 onChange={(e) => setNivel(Number(e.target.value))} 
               />
             </div>
-            <button type="submit" className="btn btn-primary">
-              Agregar Registro
-            </button>
-          </>
+          </div>
         )}
 
         {activeTab === "ubicacion" && (
-          <>
-           <div className="mb-3">
+          <div className="row">
+           <div className="col-md-6 mb-3">
               <label className="form-label">Ciudad</label>
-              <input className="form-control" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
+              <input className="form-control form-control-sm" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
             </div>
-            <div className="mb-3">
+            <div className="col-md-6 mb-3">
               <label className="form-label">Dirección</label>
-              <input className="form-control" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+              <input className="form-control form-control-sm" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
             </div>
-            <div className="mb-3">
+            <div className="col-md-6 mb-3">
               <label className="form-label">Teléfono</label>
-              <input className="form-control" value={telf} onChange={(e) => setTelf(e.target.value)} />
+              <input className="form-control form-control-sm" value={telf} onChange={(e) => setTelf(e.target.value)} />
             </div>
-          </>
+
+            {/*Botones para enviar, limpiar */}
+            <div className="d-flex justify-content-between">
+              <button type="submit" className="btn btn-success">Agregar Registro</button>
+              <button type="button" className="btn btn-secondary" onClick={limpiarCampos}>Limpiar</button>
+            </div>
+          </div>
         )}
       </form>
     </div>
--- src/pages/Estudiantes.tsx
+++ src/pages/Estudiantes.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import { Modal } from 'bootstrap';
 import FormularioEstudiante from "../components/FormularioEstudiante";
 import TablaEstudiantes from "../components/TablaEstudiantes";
 import Estadisticas from "../components/Estadisticas";
 import type { Estudiante } from "../types/Estudiante";
 
-function App() {
+function Estudiantes() {
   const [estudiantes, setEstudiantes] = useState<Estudiante[]>([]);
 
   const agregarEstudiante = (nuevo: Estudiante) => {
     setEstudiantes([...estudiantes, nuevo]);
+
+     const modal = document.getElementById("modalNuevoRegistro");
+    if (modal) {
+      const modalInstance = Modal.getInstance(modal);
+      modalInstance?.hide();
+    }
   };
 
   const total = estudiantes.length;
@@ -22,18 +29,56 @@ function App() {
     <div className="container mt-4">
       <h1 className="mb-4">Seguimiento de Estudiantes</h1>
 
-      <div className="row">
-        <div className="col-md-6">
-          <FormularioEstudiante onAgregar={agregarEstudiante} />
+       {/* Boton derecho para mostrar modal */}
+      <button
+        type="button"
+        className="btn btn-primary mb-3"
+        data-bs-toggle="modal"
+        data-bs-target="#modalNuevoRegistro"
+      >
+        Nuevo Registro
+      </button>
+
+      {/* Modal abierto */}
+      <div
+        className="modal fade"
+        id="modalNuevoRegistro"
+        tabIndex={-1}
+        aria-labelledby="modalNuevoRegistroLabel"
+        aria-hidden="true"
+      >
+         <div className="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable mt-5">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="modalNuevoRegistroLabel">
+                Nuevo Estudiante
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+
+            <div className="modal-body">
+              <FormularioEstudiante onAgregar={agregarEstudiante} />
+            </div>
+          </div>
         </div>
+      </div>
+      
+      {/*Seccion "total estudiantes" y "por carrera" */}
+      <div className="row mt-4">
         <div className="col-md-6">
           <Estadisticas total={total} porCarrera={porCarrera} />
         </div>
       </div>
 
+      {/*Seccion tabla con registros creados */}
       <TablaEstudiantes estudiantes={estudiantes} />
     </div>
   );
 }
 
-export default App;
+export default Estudiantes;
