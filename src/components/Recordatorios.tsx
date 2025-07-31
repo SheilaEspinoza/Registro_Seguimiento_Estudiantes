e4b4e26b0acf591d17713ba5a92a83a17587e63f
SheilaEspinoza <sheilaespinoza43@gmail.com>
2025-07-30 23:55:25 -0500

    Ajuste colores segun paleta

--- src/components/Recordatorios.tsx
+++ src/components/Recordatorios.tsx
@@ -68,7 +68,7 @@ interface RecordatoriosProps {
     >
       <div className="recordatorios-card">
       <div className="card-header d-flex justify-content-between align-items-center">
-        <h5 className="mb-0">Recordatorios</h5>
+        <h5 className="titulo-recordatorios">Recordatorios</h5>
           <button
             type="button"
             className="btn-morado"
